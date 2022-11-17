c587af01c6e95894cdc2cb891ee15d6c5febbe28
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2022-11-17 15:15:58 +0100

    Enabled full SimCLR augmentations

--- src/ssl_remote_sensing/pretext_tasks/simclr/augmentation.py
+++ src/ssl_remote_sensing/pretext_tasks/simclr/augmentation.py
@@ -9,7 +9,7 @@ class Augment:
     denoted x_i and  x_j which we consider a positive pair.
     """
 
-    def __init__(self, img_size, norm_means, norm_stds, s=1):
+    def __init__(self, img_size, normalize: T.Normalize, s=1):
         color_jitter = T.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
         # 10% of the image
         blur = T.GaussianBlur(
@@ -22,23 +22,18 @@ class Augment:
 
         self.train_transform = T.Compose(
             [
-                # Crop image on a random scale from 7% tpo 100%
-                # Only Use cropping since according to Wang et al. 2022
-                # it is the only augmentation that has a significant impact
+                # Crop image on a random scale from 7% to 100%
                 T.RandomResizedCrop(size=img_size),
-                # # Flip image horizontally with 50% probability
-                # T.RandomHorizontalFlip(p=0.5),
-                # # Apply heavy color jitter with 80% probability
-                # T.RandomApply([color_jitter], p=0.8),
-                # # Apply gaussian blur with 50% probability
-                # T.RandomApply([blur], p=0.5),
-                # # Convert RGB images to grayscale with 20% probability
-                # T.RandomGrayscale(p=0.2),
+                # Flip image horizontally with 50% probability
+                T.RandomHorizontalFlip(p=0.5),
+                # Apply heavy color jitter with 80% probability
+                T.RandomApply([color_jitter], p=0.8),
+                # Apply gaussian blur with 50% probability
+                T.RandomApply([blur], p=0.5),
+                # Convert RGB images to grayscale with 20% probability
+                T.RandomGrayscale(p=0.2),
+                normalize,
                 T.ToTensor(),
-                T.Normalize(
-                    mean=norm_means,
-                    std=norm_stds,
-                ),
             ]
         )
 
