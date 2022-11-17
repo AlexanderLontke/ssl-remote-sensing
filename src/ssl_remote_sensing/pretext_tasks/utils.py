42aae4d318295f0166992c2f6f8d6adf2d084b4e
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2022-11-17 15:17:32 +0100

    Added checkpoint loading to Classification_Downstream_Task.ipynb

---
+++ src/ssl_remote_sensing/pretext_tasks/utils.py
@@ -0,0 +1,20 @@
+import torch
+
+from ssl_remote_sensing.pretext_tasks.simclr.resnet_18_backbone import AddProjection
+from ssl_remote_sensing.models.ResNet18 import ResNetEncoder, resnet18_encoder
+from ssl_remote_sensing.pretext_tasks.simclr.config import get_simclr_config
+from ssl_remote_sensing.constants import RANDOM_INITIALIZATION
+
+
+def load_encoder_checkpoint_from_pretext_model(path_to_checkpoint: str) -> ResNetEncoder:
+    checkpoint = torch.load(path_to_checkpoint)
+    if "simclr" in path_to_checkpoint.lower():
+        return AddProjection(config=get_simclr_config()).load_state_dict(checkpoint).backbone
+    elif "vae" in path_to_checkpoint.lower():
+        raise NotImplementedError()
+    elif "gan" in path_to_checkpoint.lower():
+        raise NotImplementedError()
+    elif path_to_checkpoint == RANDOM_INITIALIZATION:
+        return resnet18_encoder()
+    else:
+        raise ValueError(f"Checkpoint name has to contain simclr, vae, or gan but was {path_to_checkpoint}")
