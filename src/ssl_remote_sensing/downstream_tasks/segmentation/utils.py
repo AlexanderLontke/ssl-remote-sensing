d7683f41825faf4db984381e6b861ed7bf067531
Xuemei Jiang <simonejiang.pku@gmail.com>
2022-11-18 22:09:55 +0100

    add load_encoder_checkpoint: vae

--- src/ssl_remote_sensing/downstream_tasks/segmentation/utils.py
+++ src/ssl_remote_sensing/downstream_tasks/segmentation/utils.py
@@ -3,25 +3,29 @@ import torch
 import torch.nn as nn
 from ssl_remote_sensing.pretext_tasks.vae.model import VariationalAutoencoder
 from ssl_remote_sensing.pretext_tasks.simclr.training import SimCLRTraining
+
 # from ssl_remote_sensing.pretext_tasks.simclr.config import get_simclr_config
 from ssl_remote_sensing.pretext_tasks.simclr.resnet_18_backbone import AddProjection
 
-def load_best_model(model_name, model_save_name, model_dir, train_config = None):
+
+def load_best_model(model_name, model_save_name, model_dir, train_config=None):
 
     if model_name == "VAE":
-        best_model = VariationalAutoencoder(latent_dim = train_config.latent_dim, config = train_config)
+        best_model = VariationalAutoencoder(
+            latent_dim=train_config.latent_dim, config=train_config
+        )
 
     elif model_name == "GAN":
         raise NotImplementedError()
 
     elif model_name == "SimCLR":
         raise NotImplementedError()
-        best_model = SimCLRTraining(config=train_config,feat_dim=512)
+        best_model = SimCLRTraining(config=train_config, feat_dim=512)
 
     # load state_dict from path
     best_model_path = os.path.join(model_dir, model_save_name)
     print(best_model_path)
-    state_dict_best = torch.load(best_model_path, map_location=torch.device('cpu'))
+    state_dict_best = torch.load(best_model_path, map_location=torch.device("cpu"))
     # load pre-trained models
     best_model.load_state_dict(state_dict_best)
 
@@ -29,6 +33,7 @@ def load_best_model(model_name, model_save_name, model_dir, train_config = None)
 
 
 # funciton to change first convolution layer input channels => make random kaiming normal initialization
+
 
 def patch_first_conv(encoder, new_in_channels, default_in_channels=3):
 
@@ -38,8 +43,8 @@ def patch_first_conv(encoder, new_in_channels, default_in_channels=3):
             break
 
     weight = module.weight.detach()
-    module.in_channels = new_in_channels  
-    print("New module: ", module)       
+    module.in_channels = new_in_channels
+    print("New module: ", module)
 
     new_weight = torch.Tensor(
         module.out_channels, new_in_channels // module.groups, *module.kernel_size
--- src/ssl_remote_sensing/pretext_tasks/utils.py
+++ src/ssl_remote_sensing/pretext_tasks/utils.py
@@ -3,21 +3,28 @@ import torch
 from ssl_remote_sensing.pretext_tasks.simclr.training import SimCLRTraining
 from ssl_remote_sensing.models.ResNet18 import ResNetEncoder, resnet18_encoder
 from ssl_remote_sensing.pretext_tasks.simclr.config import get_simclr_config
+from ssl_remote_sensing.pretext_tasks.vae.config import get_vae_config
 from ssl_remote_sensing.constants import RANDOM_INITIALIZATION
+from ssl_remote_sensing.pretext_tasks.vae.model import VariationalAutoencoder
 
 
 def load_encoder_checkpoint_from_pretext_model(
     path_to_checkpoint: str,
 ) -> ResNetEncoder:
     if "simclr" in path_to_checkpoint.lower():
-        return (
-            SimCLRTraining
-            .load_from_checkpoint(path_to_checkpoint, config=get_simclr_config(), feat_dim=512)
-            .model
-            .backbone
-        )
+        return SimCLRTraining.load_from_checkpoint(
+            path_to_checkpoint, config=get_simclr_config(), feat_dim=512
+        ).model.backbone
     elif "vae" in path_to_checkpoint.lower():
-        raise NotImplementedError()
+
+        best_model = VariationalAutoencoder(
+            latent_dim=get_simclr_config().latent_dim, config=get_simclr_config()
+        )
+        state_dict_best = torch.load(
+            path_to_checkpoint, map_location=torch.device("cpu")
+        )
+        best_model.load_state_dict(state_dict_best)
+        return best_model.encoder
     elif "gan" in path_to_checkpoint.lower():
         raise NotImplementedError()
     elif path_to_checkpoint == RANDOM_INITIALIZATION:
