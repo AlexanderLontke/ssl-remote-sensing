bdf945d649dc67a411f00935968cafca925b1311
marccgrau <46031196+marccgrau@users.noreply.github.com>
2022-12-11 12:41:33 +0100

    train transform adjustment

--- src/ssl_remote_sensing/data/bigearthnet_dataloader.py
+++ src/ssl_remote_sensing/data/bigearthnet_dataloader.py
@@ -5,10 +5,11 @@ from pytorch_lightning import LightningDataModule
 from ssl_remote_sensing.data.bigearthnet_dataset import Bigearthnet
 from torch.utils.data import DataLoader
 
+ALL_BANDS = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B11', 'B12']
 
 class BigearthnetDataModule(LightningDataModule):
 
-    def __init__(self, data_dir, bands=None, batch_size=32, num_workers=16, seed=42):
+    def __init__(self, data_dir, bands=ALL_BANDS, batch_size=32, num_workers=16, seed=42, train_transforms=None):
         super().__init__()
         self.data_dir = data_dir
         self.bands = bands
@@ -16,6 +17,7 @@ class BigearthnetDataModule(LightningDataModule):
         self.num_workers = num_workers
         self.seed = seed
         self.train_dataset = None
+        self.train_transforms = train_transforms
 
     def setup(self):
         train_transforms = self.train_transform() if self.train_transforms is None else self.train_transforms
