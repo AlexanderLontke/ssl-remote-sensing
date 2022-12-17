58127348929e0bd739c35a7aea514d018db5ada7
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2022-12-17 18:36:57 +0100

    Fixed util method for random init

--- src/ssl_remote_sensing/pretext_tasks/utils.py
+++ src/ssl_remote_sensing/pretext_tasks/utils.py
@@ -44,7 +44,7 @@ def load_encoder_checkpoint_from_pretext_model(
         model.load_state_dict(state_dict_best)
         return model
     elif (
-        path_to_checkpoint == "/content/drive/MyDrive/deep_learning_checkpoints/random"
+        "random" in path_to_checkpoint
     ):
         return resnet18_encoder()
     else:
